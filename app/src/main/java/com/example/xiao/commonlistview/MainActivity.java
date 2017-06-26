f465bee8ecfe73e195fcca48db89eec581c62e35
zhangxiao <zhangxiao@yixue2015.com>
2017-06-26 18:55:14 +0800

    格式化代码

--- app/src/main/java/com/example/xiao/commonlistview/MainActivity.java
+++ app/src/main/java/com/example/xiao/commonlistview/MainActivity.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<ContactsBean> mList=new ArrayList<>();
-//    private ContactsAdapter mAdapter;
+    private List<ContactsBean> mList = new ArrayList<>();
+    //    private ContactsAdapter mAdapter;
     private CommonAdapter mAdapter;
     private android.widget.ListView lv;
     private android.widget.RelativeLayout activitymain;
@@ -23,18 +23,20 @@ public class MainActivity extends AppCompatActivity {
         this.activitymain = (RelativeLayout) findViewById(R.id.activity_main);
         this.lv = (ListView) findViewById(R.id.lv);
 //        lv.setAdapter(mAdapter=new ContactsAdapter(this,mList));
-        lv.setAdapter(mAdapter= new CommonAdapter<ContactsBean>(this,mList, R.layout.item_layout) {
+        lv.setAdapter(mAdapter = new CommonAdapter<ContactsBean>(this, mList, R.layout.item_layout) {
             @Override
             public void convertView(ViewHolder holder, ContactsBean contactsBean) {
-                holder.set(R.id.name,contactsBean.getName()).set(R.id.phone_number,contactsBean.getPhoneNumber()).set(R.id.head_img,contactsBean.getHead_img());
+                holder.set(R.id.name, contactsBean.getName())
+                        .set(R.id.phone_number, contactsBean.getPhoneNumber())
+                        .set(R.id.head_img, contactsBean.getHead_img());
             }
         });
         initData();
     }
 
-    public void initData(){
-        for (int i=0;i<20;i++){
-            ContactsBean bean=new ContactsBean();
+    public void initData() {
+        for (int i = 0; i < 20; i++) {
+            ContactsBean bean = new ContactsBean();
             bean.setName("小明");
             bean.setPhoneNumber("110");
             bean.setHead_img("http://img.qqai.net/uploads/i_2_1826721258x2403292640_21.jpg");
